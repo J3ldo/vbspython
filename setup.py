341d07d78fb517441a78f8123db59451b6d56be3
Jelle  Jeeninga <jelle7664@lindecollege.nl>
2022-02-02 16:41:17 +0100

    :(

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Education',
-    'Operating System :: Microsoft :: Windows :: Windows 7/8/10/11',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3'
 ]
@@ -12,10 +12,10 @@ setup(
     name='vbpython',
     version='0.0.2',
     description='A python module to make .vbs code',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description=open('packageinformation/README.txt').read() + '\n\n' + open('packageinformation/CHANGELOG.txt').read(),
     url='',
     author='J3ldo',
-    author_email='Please use discord Jeldo#9587',
+    author_email='',
     license='MIT',
     classifiers=classifiers,
     keywords='vbscript vbs ',
